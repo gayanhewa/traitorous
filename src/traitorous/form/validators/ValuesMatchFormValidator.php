83fd2d550ab55aeebbfb4986e3f185c9fd07c538
Joseph Moniz <joseph.moniz@gmail.com>
2014-04-02 22:15:54 -0700

    Created a value match form validator
    This form validator takes two keys and validates that their values match
    within the supplied form data.

---
+++ src/traitorous/form/validators/ValuesMatchFormValidator.php
@@ -0,0 +1,37 @@
+<?hh // strict
+namespace traitorous\form\validators;
+
+use traitorous\form\errors\GeneralFormError;
+use traitorous\form\FormErrors;
+use traitorous\form\FormValidator;
+use traitorous\matcher\string\StringRegexMatcher;
+use traitorous\Option;
+use traitorous\option\OptionFactory;
+use traitorous\Validation;
+use traitorous\validation\Failure;
+use traitorous\validation\Success;
+
+final class ValuesMatchFormValidator implements FormValidator {
+
+    public function __construct(
+        private string $_k1,
+        private string $_k2,
+        private string $_errorMessage
+    ) { }
+
+    public function validate(Map<string, string> $data): Validation<FormErrors, bool> {
+        $tuple = $this->_getTuplePair($data);
+
+        return $tuple->filter(((string, string) $t) ==> $t[0] == $t[1])->cata(
+            ()   ==> new Failure(new FormErrors(Vector { new GeneralFormError($this->_errorMessage) })),
+            ($_) ==> new Success(true)
+        );
+    }
+
+    private function _getTuplePair(Map<string, string> $data): Option<(string, string)> {
+        return OptionFactory::fromValue($data->get($this->_k1))->flatMap(($v1) ==> 
+            OptionFactory::fromValue($data->get($this->_k2))->map(($v2) ==> tuple($v1, $v2))
+        );
+    }
+
+}
